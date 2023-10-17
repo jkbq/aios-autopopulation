700a3fa18e2fb238408280214461851ed65d6db0
rowell.blanca <rowell.blanca@august99.com>
2023-10-17 12:26:54 +0800

    Add menu api

--- routes/generateMenu.class.php
+++ routes/generateMenu.class.php
@@ -16,22 +16,66 @@ class Menu {
 
     public function aios_populate_menu($data) {
 
-        // Your logic to generate a page goes here
-        $post_data = array(
-            'post_title'    => 'Generated Page',
-            'post_content'  => 'This is the content of the generated page.',
-            'post_status'   => 'publish',
-            'post_type'     => 'page',
-        );
+        
+        $jsonData = AIOS_AUTOPOPULATE_JSON . 'config.json';
 
-        $post_id = wp_insert_post($post_data);
+        $response = wp_remote_get($jsonData, array(
+            'timeout' => 45,
+            'blocking' => true,
+            'cookies' => array()
+        ));
 
-        if ($post_id) {
-            $response = array('success' => true, 'message' => 'Page generated successfully.', 'post_id' => $post_id);
-        } else {
-            $response = array('success' => false, 'message' => 'Error generating page.');
+        $data =  json_decode($response['body']);
+
+        $menus = $data->menu[0];
+
+        foreach ($menus as $menu) {
+            foreach ($menu as $menu_data) {
+
+                $location = $menu_data->location;
+                $menu_name = $menu_data->menu_name;
+                $nav_items = $menu_data->nav;
+
+                // Check if the menu exists by location
+                $menu_exists = wp_get_nav_menu_object($menu_name);
+
+                if (!$menu_exists) {
+                    if (isset($nav_items)) {
+                        $menu_id = wp_create_nav_menu($menu_name); // Use the actual menu name
+                        $locations = get_theme_mod('nav_menu_locations');
+                        $locations[$location] = $menu_id;
+                        set_theme_mod('nav_menu_locations', $locations);
+
+                        $parent_id_arr = array(0);
+                        $parent_id = 0;
+
+                        foreach ($nav_items as $json_data) {
+                            if ($json_data->post_type == 'custom-navigation') {
+                                $parent_id = wp_update_nav_menu_item(
+                                    $menu_id,
+                                    0,
+                                    array(
+                                        'menu-item-title' => __($json_data->title),
+                                        'menu-item-url' => home_url($json_data->url),
+                                        'menu-item-status' => 'publish',
+                                        'menu-item-parent-id' => $parent_id_arr[$json_data->parent]
+                                    )
+                                );
+                                $count_menu++;
+                            }
+                            // Set parent and child
+                            $parent_id_arr[$json_data->id] = $parent_id;
+                        }
+                    }
+                    $response = array( 'message' => 'Menu and items generated successfully.');
+
+                }else{
+                    $response = array( 'message' => 'Menu already exists.');
+                }
+            }
         }
 
         return rest_ensure_response($response);
     }
 }
+new Menu();
