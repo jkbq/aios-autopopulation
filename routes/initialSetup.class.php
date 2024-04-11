f6abb865fadb6cc9b15b485c0887cbabbea6ab4c
rowell.blanca <rowell.blanca@august99.com>
2024-04-11 10:58:41 +0800

    fixes

--- routes/initialSetup.class.php
+++ routes/initialSetup.class.php
@@ -20,12 +20,31 @@ class InitialSetupPage {
 
         $initialSetupPages = get_option( 'aios_auto_population_default_pages', false );
 
+
+        $url =  get_stylesheet_directory_uri() .'/config.json';
+
+        $response = wp_remote_get($url, array(
+            'timeout' => 45,
+            'blocking' => true,
+            'cookies' => array()
+        ));
+
+        $data =  json_decode($response['body']);
+
+        $client_info = $data->config[0]->site_info;
+
         if (!$initialSetupPages) {
             // Initial setup required pages
             $initial_required = AIOS_INITIAL_SETUP_DIR . DIRECTORY_SEPARATOR . 'backward-compatibility' . DIRECTORY_SEPARATOR . 'generate-default-pages.php';
 
             if (file_exists($initial_required) && include_once $initial_required) {
-                $ids = [0, 1, 2, 4, 5]; // Default IDs
+
+                $ids = [];
+                if(isset($client_info->has_contact_form)){
+                    $ids = [0, 1, 2, 3, 4, 5];
+                }else{
+                    $ids = [0, 1, 2, 4, 5];
+                }
 
                 $aios_initial_setup_generate_default_pages = new aios_initial_setup_generate_default_pages();
                 $aios_initial_setup_generate_default_pages->generate_default_pages($ids);
