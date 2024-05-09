e65be47d544429295f498debaeab1ca867b7bf31
rowell.blanca <rowell.blanca@august99.com>
2024-05-10 06:09:37 +0800

    update initial setup default pages

--- routes/initialSetup.class.php
+++ routes/initialSetup.class.php
@@ -38,14 +38,12 @@ class InitialSetupPage {
             $initial_required = AIOS_INITIAL_SETUP_DIR . DIRECTORY_SEPARATOR . 'backward-compatibility' . DIRECTORY_SEPARATOR . 'generate-default-pages.php';
 
             if (file_exists($initial_required) && include_once $initial_required) {
-
                 $ids = [];
                 if(isset($client_info->has_contact_form)){
                     $ids = [0, 1, 2, 3, 4, 5];
                 }else{
                     $ids = [0, 1, 2, 3, 4, 5];
                 }
-
                 $aios_initial_setup_generate_default_pages = new aios_initial_setup_generate_default_pages();
                 $aios_initial_setup_generate_default_pages->generate_default_pages($ids);
 
