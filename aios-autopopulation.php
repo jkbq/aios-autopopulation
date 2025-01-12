e019078eb3f50ee33946da429991b691cf50b5ed
Bryan Suarez <bryan.suarez@august99.com>
2025-01-13 06:40:42 +0800

    Add slideshow video support

--- aios-autopopulation.php
+++ aios-autopopulation.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: AIOS Auto Population
  * Description: Autopopulate Agentpro and AIX Theme
- * Version: 1.2.9
+ * Version: 1.3.0
  * Author: Agent Image
  * Author URI: https://www.agentimage.com/
  * License: Proprietary
