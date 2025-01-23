1254f8ccbbb77e3e28fed3c8912ecc395cc48c0b
rowell.blanca <rowell.blanca@august99.com>
2025-01-24 06:20:12 +0800

    Add option for category description

--- aios-autopopulation.php
+++ aios-autopopulation.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: AIOS Auto Population
  * Description: Autopopulate Agentpro and AIX Theme
- * Version: 1.3.3
+ * Version: 1.3.4
  * Author: Agent Image
  * Author URI: https://www.agentimage.com/
  * License: Proprietary
