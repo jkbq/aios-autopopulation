303f36ea1cd63630a9e652820c4ff3ab6729c526
Rowell Blanca <rowell.blanca@august99.com>
2025-03-18 06:31:16 +0800

    update query for aios testimonials

--- aios-autopopulation.php
+++ aios-autopopulation.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: AIOS Auto Population
  * Description: Autopopulate Agentpro and AIX Theme
- * Version: 1.3.8
+ * Version: 1.3.9
  * Author: Agent Image
  * Author URI: https://www.agentimage.com/
  * License: Proprietary
