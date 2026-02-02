c72b10f08f0bb81a404af18729a363f25d811bf9
Rowell Blanca <rowell.blanca@august99.com>
2026-02-02 16:08:28 +0800

    add support for theme switch for mobile theme

--- aios-autopopulation.php
+++ aios-autopopulation.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: AIOS Auto Population
  * Description: Autopopulate Agentpro and AIX Theme
- * Version: 2.0.11
+ * Version: 2.0.12
  * Author: Agent Image
  * Author URI: https://www.agentimage.com/
  * License: Proprietary
