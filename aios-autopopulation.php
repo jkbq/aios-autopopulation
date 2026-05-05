58e312dda17e617fbf60ba7846dfb3fd1232f5d1
Earl Loyola <earlrico16@gmail.com>
2026-05-05 10:06:34 +0800

    feat/add additional support for testimonials generation

--- aios-autopopulation.php
+++ aios-autopopulation.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: AIOS Auto Population
  * Description: Autopopulate Agentpro and AIX Theme
- * Version: 2.0.15
+ * Version: 2.1.0
  * Author: Agent Image
  * Author URI: https://www.agentimage.com/
  * License: Proprietary
