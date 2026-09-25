b5586693af479d7044babef3e99867160bcafe84
Bryan Suarez <bryan.suarez@august99.com>
2026-09-25 12:06:05 +0800

    Strip hidden titles from populated post content - Apply aios-content-templates/strip_hidden_titles filter when syncing post_content - Bump version to 2.5.1

--- aios-autopopulation.php
+++ aios-autopopulation.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: AIOS Auto Population
  * Description: Autopopulate Agentpro and AIX Theme
- * Version: 2.5.0
+ * Version: 2.5.1
  * Author: Agent Image
  * Author URI: https://www.agentimage.com/
  * License: Proprietary
